e66fe6595ae05d5d0f01f17e8db0b4ac00ffd89e
Cool Person <121198303+coolloo@users.noreply.github.com>
2025-10-30 20:42:34 +0000

    finished main game

--- script.js
+++ script.js
@@ -1,1 +1,79 @@
-// add javascript here
+// time
+date.textContent = time();
+
+// global variables
+let score, answer, level;
+const levelArr = document.getElementsByName("level");
+const scoreArr = [];
+
+// event listener
+playBtn.addEventListener("click", play);
+guessBtn.addEventListener("click", makeGuess);
+
+function time(){
+    let d = new Date();
+    // concatenate date
+    let str = d.getMonth()+1 + "/" + d.getDate() + "/" + d.getFullYear();
+    // update here
+    return str;
+}
+function play(){
+    playBtn.disabled = true;
+    guessBtn.disabled = false;
+    guess.disabled = false;
+    for(let i=0; i<levelArr.length; i++){
+        levelArr[i].disabled = true;
+        if(levelArr[i].checked){
+            level = levelArr[i].value;
+        }
+    }
+    answer = Math.floor(Math.random()*level)+1;
+    msg.textContent = "Guess a number between 1-" + level;
+    guess.placeholder = answer;
+    score = 0;
+}
+function makeGuess(){
+    let userGuess = parseInt(guess.value);
+    if(isNaN(userGuess) || userGuess < 1 || userGuess > level){
+        msg.textContent = "Invalid, guess a number!";
+        return;
+    }
+    score++;
+    if(userGuess < answer){
+        msg.textContent = "Too low, guess again.";
+    }
+    else if(userGuess > answer){
+        msg.textContent = "Too high, guess again.";
+    }
+    else{
+        msg.textContent = "Correct! It took " + score + " tries.";
+        updateScore();
+        reset();
+    }
+}
+function reset(){
+    guessBtn.disabled = true;
+    guess.value = "";
+    guess.placeholder = "";
+    guess.disabled = true;
+    playBtn.disabled = false;
+    for(let i=0; i<levelArr.length; i++){
+        levelArr[i].disabled = false;
+    }
+}
+function updateScore(){
+    scoreArr.push(score);
+    wins.textContent = "Total wins: " + scoreArr.length;
+    let sum = 0;
+    scoreArr.sort((a, b) => a - b);
+    const lb = document.getElementsByName("leaderboard");
+
+    for(let i=0; i<scoreArr.length; i++){
+        sum += scoreArr[i];
+        if(i < lb.length){
+            lb[i].textContent = scoreArr[i];
+        }
+    }
+    let avg = sum/scoreArr.length;
+    avgScore.textContent = "Average Score: " + avg.toFixed(2);
+}
